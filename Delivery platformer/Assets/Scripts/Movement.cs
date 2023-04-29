9192dbe1ea34bd43c01fff6d7c3374127804ec40
Podrochimaru <kostya.arn@gmail.com>
2023-04-29 22:05:33 +0300

    Add movement
    Add forward, backward and jump

--- Delivery platformer/Assets/Scripts/Movement.cs
+++ Delivery platformer/Assets/Scripts/Movement.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D _rigidbody;
+    private float _speed = 3.0f;
+    private bool _onGround = true;
+    private float _horizontal;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _onGround = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _onGround = false;
+    }
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.velocity = new Vector2(_speed, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+
+            _rigidbody.velocity = new Vector2(-_speed, 0);
+        }
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _onGround)
+        {
+            _rigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && _onGround)
+        {
+            _rigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
     }
 }
